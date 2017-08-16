ce72d5c87c494fd3b049ccf75dead2608aebb059
georgiah <georgia+github@fastmail.com>
2017-08-16 17:49:43 +1000

    Styled log in screen

--- templates/login/index.js
+++ templates/login/index.js
@@ -22,29 +22,107 @@ function login (state, emit) {
 
   var style = css`
     :host {
-      background-color: #3399ff;
+      font-family: Arial;
+      font-size: 20px;
+    }
+
+    :host > .banner {
+      background-color: #49654C;
+      height: 4rem;
+      margin-bottom: 1rem;
+    }
+
+    .form-field {
+      margin-bottom: 1rem;
+    }
+
+    input[type=text], input[type=password] {
+      border: 0px;
+      border-bottom: 1px solid black;
+      font-size: 20px;
+      margin-left: 10%;
+      width: 80%;
+    }
+
+    .wrapper {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      text-align: center;
+    }
+
+    input[type=checkbox] {
+      height: 20px;
+      vertical-align: middle;
+      width: 20px;
+    }
+
+    button {
+      background-color: #8AA989;
+      border: none;
+      border-radius: 2px;
+      color: #EBEBE9;
+      font-size: 20px;
+      height: 2.5rem;
+      margin-left: 15%;
+      margin-bottom: 1rem;
+      width: 70%;
+    }
+
+    .submit { background-color: #C0CEB2; }
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+
+    .warning {
+      display: flex;
+      justify-content: centre;
+      margin-left: 10%;
+      width: 80%;
+    }
+
+    .warning > p {
+      font-size: 14px;
+      margin: 0;
+      margin-bottom: 1rem;
+    }
+
+    .spinner {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      margin-bottom: 2rem;
     }
   `
 
   return html`
     <div class=${style}>
+      <div class="banner"></div>
       <div class="form-field">
         <input type="text" id="email" value=${email} placeholder="email address" oninput=${updateInput} />
-        <input type="checkbox" /> Remember Me
+      </div>
+      <div class="form-field wrapper">
+        <label>remember my email
+          <input type="checkbox" />
+        </label>
       </div>
       <div class="form-field">
         <input type="password" id="password" value=${password} placeholder="password" oninput=${updateInput} />
       </div>
       <div>
-        ${loading ? spinner() : html`<button class="submit" onclick=${submit}>Log In</button>`}
-        ${error(state, emit)}
+        ${loading ? spinner() : html`<button class="submit" onclick=${submit}>log in</button>`}
       </div>
+      ${error(state, emit)}
       <div>
-        <a href="/forgottenPassword">I forgot my password</a>
+        <button>
+          <a href="/forgottenPassword">i forgot my password</a>
+        </button>
       </div>
     </div>
   `
-  
+
   function updateInput (e) {
     var id = e.target.id
     var text = e.target.value
@@ -65,7 +143,7 @@ function login (state, emit) {
         emit('errorClear')
         emit('pushState', '/resetPassword')
       } else if (email !== 'success@test.net') {
-        emit('error', 'The email address or password you have entered is incorrect, please try again')
+        emit('error', `the email address or password you've entered is incorrect, please try again (hint: try success@test.net or reset@test.net)`)
       } else {
         emit('errorClear')
         emit('pushState', '/home')
--- templates/spinner/index.js
+++ templates/spinner/index.js
@@ -4,5 +4,8 @@ var css = require('sheetify')
 css('./spinner.css')
 
 module.exports = function () {
-  return html`<div class="sp sp-circle"></div>`
+  return html`
+    <div class="spinner">
+      <div class="sp sp-circle"></div>
+    </div>`
 }
