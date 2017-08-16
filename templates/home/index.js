af0306fa1859cdf1633b87fbef591a551cd1ffcf
georgiah <georgia+github@fastmail.com>
2017-08-16 20:41:58 +1000

    Simple home screen mock up

--- templates/home/index.js
+++ templates/home/index.js
@@ -1,26 +1,17 @@
-// TODO
-// styling
-
 // require dependencies
 var html = require('choo/html')
-var css = require('sheetify')
 
 // export module
 module.exports = home
 
 // declare templates
 function home (state, emit) {
-  var style = css`
-    :host {
-      background-color: #3399ff;
-      color: white;
-    }
-  `
 
   return html`
-    <div class=${style}>
-      <h2>Welcome to your home screen ${state.user.email} :)</h2>
-      <p>You have been successfully logged in</p>
+    <div>
+      <div class="banner"></div>
+      <div class="wrapper">welcome to your home screen ${state.user.email} :)</div>
+      <div class="message">you have been successfully logged in</div>
     </div>
     `
 }
